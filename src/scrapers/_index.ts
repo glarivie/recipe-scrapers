9d5d34f3ea88a1baaa6d1b393c016f6a6f2a5188
Guillaume L. <guillaumel@alvie.fr>
2026-02-21 17:20:02 +0100

    feat: add scrapers for Cookpad, Tasty, Bon Appétit, Delish, Food52

--- src/scrapers/_index.ts
+++ src/scrapers/_index.ts
@@ -2,11 +2,15 @@ import type { AbstractScraper } from '@/abstract-scraper'
 import { AllRecipes } from './allrecipes'
 import { AmericasTestKitchen } from './americastestkitchen'
 import { BBCGoodFood } from './bbcgoodfood'
+import { BonAppetit } from './bonappetit'
 import { BudgetBytes } from './budgetbytes'
+import { CookPad } from './cookpad'
 import { DamnDelicious } from './damndelicious'
+import { Delish } from './delish'
 import { EatingWell } from './eatingwell'
 import { Epicurious } from './epicurious'
 import { Food } from './food'
+import { Food52 } from './food52'
 import { HalfBakedHarvest } from './halfbakedharvest'
 import { Marmiton } from './marmiton'
 import { NYTimes } from './nytimes'
@@ -14,6 +18,7 @@ import { RecipeTinEats } from './recipetineats'
 import { SeriousEats } from './seriouseats'
 import { SimplyRecipes } from './simplyrecipes'
 import { SkinnyTaste } from './skinnytaste'
+import { Tasty } from './tasty'
 
 /**
  * A map of all scrapers.
@@ -22,11 +27,15 @@ export const scrapers = {
   [AllRecipes.host()]: AllRecipes,
   [AmericasTestKitchen.host()]: AmericasTestKitchen,
   [BBCGoodFood.host()]: BBCGoodFood,
+  [BonAppetit.host()]: BonAppetit,
   [BudgetBytes.host()]: BudgetBytes,
+  [CookPad.host()]: CookPad,
   [DamnDelicious.host()]: DamnDelicious,
+  [Delish.host()]: Delish,
   [EatingWell.host()]: EatingWell,
   [Epicurious.host()]: Epicurious,
   [Food.host()]: Food,
+  [Food52.host()]: Food52,
   [HalfBakedHarvest.host()]: HalfBakedHarvest,
   [Marmiton.host()]: Marmiton,
   [NYTimes.host()]: NYTimes,
@@ -34,4 +43,5 @@ export const scrapers = {
   [SeriousEats.host()]: SeriousEats,
   [SimplyRecipes.host()]: SimplyRecipes,
   [SkinnyTaste.host()]: SkinnyTaste,
+  [Tasty.host()]: Tasty,
 } as const satisfies Record<string, typeof AbstractScraper>
